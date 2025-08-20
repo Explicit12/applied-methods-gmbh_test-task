f9f91cc3837cf2a02f1d600d9fa38141f37fa473
Explicit12 <explicit1@tutanota.com>
2025-08-20 12:32:05 +0300

    Added vitest for unit-tests

---
+++ src/tests/test.test.ts
@@ -0,0 +1,11 @@
+import { describe, test, expect } from "vitest";
+
+describe("Test Suite", () => {
+  test("Test Case 1", () => {
+    expect(true).toBe(true);
+  });
+
+  test("Test Case 2", () => {
+    expect(false).toBe(false);
+  });
+});
