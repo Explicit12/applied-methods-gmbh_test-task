1c06e8f5c9d9c49820b34bd9e808cf328b764b12
Explicit12 <explicit1@tutanota.com>
2025-08-20 12:17:25 +0300

    init

---
+++ eslint.config.ts
@@ -0,0 +1,50 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
+import stylistic from "@stylistic/eslint-plugin";
+import pluginPrettier from "eslint-config-prettier/flat";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  pluginPrettier,
+  {
+    plugins: {
+      "@stylistic": stylistic,
+    },
+    rules: {
+      "@stylistic/semi": ["warn", "always"],
+      "@stylistic/quotes": ["warn", "double"],
+      "@stylistic/max-len": ["warn", { code: 120 }],
+      "unicorn/filename-case": ["error", { cases: { pascalCase: true, camelCase: true } }],
+    },
+  },
+  {
+    plugins: {
+      "@stylistic": stylistic,
+    },
+    rules: {
+      "@stylistic/indent": ["warn", 2],
+    },
+    ignores: ["**/*.vue"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  eslintPluginUnicorn.configs.recommended,
+  tseslint.configs.recommended,
+  pluginVue.configs["flat/essential"],
+  {
+    files: ["**/*.vue"],
+    rules: {
+      "vue/script-indent": ["warn", 2, { baseIndent: 1 }],
+    },
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
+  },
+
+  globalIgnores([".github/*", ".vscode/*", "node_modules/*"]),
+]);
